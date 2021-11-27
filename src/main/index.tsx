6dd4d4cd0d7fb9fc01178baccb2131e3f0969989
Vitor Thomazini <vitor.thomazini94@gmail.com>
2021-11-27 19:12:14 -0300

    chore: add eslint-plugin-react

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import Style from '@/presentation/styles/global.scss';
+import Style from '@/presentation/styles/global.scss'
 
 ReactDOM.render(
-  <h1 className={Style.main}>Hello World</h1>, 
+  <h1 className={Style.main}>Hello World</h1>,
   document.getElementById('main')
 )
